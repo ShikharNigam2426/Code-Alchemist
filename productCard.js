b774662a0c5d68ec36948a048cb840b79d183e47
Madhur2426 <shikharnigam123@gmail.com>
2024-06-28 23:42:23 +0530

    made women and men product section

---
+++ productCard.js
@@ -0,0 +1,122 @@
+const femaleProducts = [
+    {
+        img1: "https://imgs.search.brave.com/33acBFCbyBp4Z9aG9LyGpa0h6eOiP2Vm1b9GIrLcWV0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEwZ1hwZ3ErV0wu/anBn",
+        img2: "./images/women-col/red.jpeg",
+        description: "Smocked-bodice dress",
+        price: "Rs.1,299.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/QgqKrOtdmstvvawG3ykt5RHLVlRDMMaVYbAxV47QOvk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFLd1hMbDZBdUwu/anBn",
+        img2: "./images/women-col/blk-col-mol.jpeg",
+        description: "Overlock-detail-dress",
+        price: "Rs.1,299.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/XxmnySYPCsBW7h6wWH2ldJ9fn_sRlhH-_XMHKDCyBEs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFpTlNMNG5zeUwu/anBn",
+        img2: "./images/women-col/button-mol.webp",
+        description: "Button-top vest top",
+        price: "RS.5,000.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/C0nksxqDqVTP9CR84b1hBC4iZGSoQZUDKt5ij7sRtVU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEtb3B1ZVVUTUwu/anBn",
+        img2: "./images/women-col/gray-col-mol.jpeg",
+        description: "Dress with Flared Skirt",
+        price: "Rs.4,000.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/C0nksxqDqVTP9CR84b1hBC4iZGSoQZUDKt5ij7sRtVU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEtb3B1ZVVUTUwu/anBn",
+        img2: "./images/women-col/gray-col-mol.jpeg",
+        description: "Dress with Flared Skirt",
+        price: "Rs.4,000.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/C0nksxqDqVTP9CR84b1hBC4iZGSoQZUDKt5ij7sRtVU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEtb3B1ZVVUTUwu/anBn",
+        img2: "./images/women-col/gray-col-mol.jpeg",
+        description: "Dress with Flared Skirt",
+        price: "Rs.4,000.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/C0nksxqDqVTP9CR84b1hBC4iZGSoQZUDKt5ij7sRtVU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEtb3B1ZVVUTUwu/anBn",
+        img2: "./images/women-col/gray-col-mol.jpeg",
+        description: "Dress with Flared Skirt",
+        price: "Rs.4,000.00"
+    },
+    {
+        img1: "https://imgs.search.brave.com/C0nksxqDqVTP9CR84b1hBC4iZGSoQZUDKt5ij7sRtVU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEtb3B1ZVVUTUwu/anBn",
+        img2: "./images/women-col/gray-col-mol.jpeg",
+        description: "Dress with Flared Skirt",
+        price: "Rs.4,000.00"
+    }
+];
+
+const maleProduct = [
+    {
+        img1: "https://imgs.search.brave.com/Ym3V9E7Vxva-tkG51ouMeOW1DYLbLZjubYt48rTg38M/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFXb1dRenZvcEwu/anBn",
+        img2: "./images/women-col/men1mol.jpeg",
+        description: " linen-blend trousers",
+        price: "Rs.2,699.00",
+    },
+    {
+        img1: "https://imgs.search.brave.com/r2NH_gxI-myjDB4Y79n4EDg2o4Fewl_tE5udeN4Stf4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZmFzaGlvbGEu/aW4vcHJvZHVjdC1s/aXN0LzMwMHg0NTAv/aC1hbmQtbS8xMDgw/MzA1NDcvMy1wYWNr/LXJlZ3VsYXItZml0/LXQtc2hpcnRzLndl/YnA",
+        description: "Loose White Fit T-shirt",
+        price: "Rs.1,299.00",
+    },
+    {
+        img1: "https://imgs.search.brave.com/XzfUT83t3R-rrnqhKMbRJbs62xzzMR64P0UTYjfKb30/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9scDIu/aG0uY29tL2htZ29l/cHByb2Q_c2V0PXNv/dXJjZVsvNjkvZTcv/NjllN2Q2YTRkOTlm/MTZkOTYwNTgxNTQy/NGI0MjIwYjU4OWUz/ZTIxNC5qcGddLG9y/aWdpbltkYW1dLGNh/dGVnb3J5W21lbl90/c2hpcnRzdGFua3Nf/cHJpbnRlZF0sdHlw/ZVtERVNDUklQVElW/RVNUSUxMTElGRV0s/cmVzW21dLGhtdmVy/WzJdJmNhbGw9dXJs/W2ZpbGU6L3Byb2R1/Y3Qvc3R5bGVd.jpeg",
+        img2: "./images/women-col/lemon-m3.jpeg",
+        description: "Fit Crinkled resort shirt",
+        price: "Rs.1,999.00",
+    },
+    {
+        img1: "https://imgs.search.brave.com/S6uiw_ykSfFV8_n9Bdh1rgCGN6lCmi8nOTFUwJLMV3g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFqWTAtRHZTbUwu/anBn",
+        img2: "./images/women-col/reg-t-mol.jpeg",
+        description: "Regular White Fit T-shirt",
+        price: "Rs.599.00",
+    }];
+
+const femaleProductSection = document.getElementsByClassName('female-product')[0];
+
+const container = femaleProducts.map((product) => {
+    return `
+        <div class="col-lg-3 col-md-6 py-2 col-sm-6 feature-card-column">
+            <div class="box feature-card">
+                <div class="feature-card-image">
+                    <img class="px-1 py-1" src="${product.img1}" alt="Feature Image" />
+                </div>
+                <div class="feature-card-content pb-2">
+                    <p class="grey">${product.description}</p>
+                </div>
+                <hr class="rule" />
+                <div class="feature-card-heading pt-3">
+                    <h1 class="grey">${product.price}</h1>
+                </div>
+            </div>
+        </div>
+    `;
+}).join('');
+
+femaleProductSection.innerHTML = container;
+
+
+const maleProductSection = document.getElementsByClassName('male-product')[0];
+
+const maleContainer = maleProduct.map((product) => {
+    return `
+        <div class="col-lg-3 col-md-6 col-sm-6 feature-card-column">
+            <div class="box feature-card">
+                <div class="feature-card-image">
+                    <img class="px-1 py-1" src="${product.img1}" alt="Feature Image" />
+                </div>
+                <div class="feature-card-content pb-2">
+                    <p class="grey">${product.description}</p>
+                </div>
+                <hr class="rule" />
+                <div class="feature-card-heading pt-3">
+                    <h1 class="grey">${product.price}</h1>
+                </div>
+            </div>
+        </div>
+    `;
+}).join('');
+
+maleProductSection.innerHTML = maleContainer;
